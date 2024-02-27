e6e4114e93516b7edfa1fc0fb1da6d3cb78c962c
Christopher Buckley <topherbuckley@gmail.com>
2024-02-27 15:14:26 +0900

    Movig to an OOP motor implementation

--- motor_driver_test.py
+++ motor_driver_test.py
@@ -2,29 +2,41 @@ import time
 import math
 import Jetson.GPIO as GPIO
 
+class Motor:
+    def __init__(self, pwm_pin: int, in1: int, in2: int, _frequency_hz: int) -> None:
+        self.pwm_pin: int = pwm_pin
+        self.in1: int = in1
+        self.in2: int = in2
+        self._frequency_hz: int = _frequency_hz
+        self._duty_cycle_percent: int = 0  # Initialized to 0, and int can hold -100 to 100 inclusively
+        # set pin as an output pin with optional initial state of LOW
+        GPIO.setup(self.pwm_pin, GPIO.OUT, initial=GPIO.LOW)
+        # p1 is a PWM object.
+        self.pwm = GPIO.PWM(self.pwm_pin, self._frequency_hz)
+        self.pwm.start(self._duty_cycle_percent)
+
+    def change_speed(self, duty_cycle_percent: int) -> None:
+        # I only add this as we may want to use it as a state variable later
+        self._duty_cycle_percent = duty_cycle_percent
+        self.pwm.ChangeDutyCycle(self._duty_cycle_percent)
+    def turn_off(self) -> None:
+        self.pwm.stop()
+
 
 def main():
     pwm_pins = [32,33]
-    # Pin Setup:
+    gpio_pins = [7,15,29,31]
+    _frequency_hz = 50
+
     # Board pin-numbering scheme
     GPIO.setmode(GPIO.BOARD)
+    
+    motor1 = Motor(pwm_pins[0], gpio_pins[0], gpio_pins[1], _frequency_hz)
+    motor2 = Motor(pwm_pins[1], gpio_pins[2], gpio_pins[3], _frequency_hz)
 
-    _frequency_hz = 50
+
     _duty_cycle_percent = 25
     incr = 5
-
-    # set pin as an output pin with optional initial state of HIGH
-    GPIO.setup(pwm_pins[0], GPIO.OUT, initial=GPIO.HIGH)
-    # p1 is a PWM object.
-    p1 = GPIO.PWM(pwm_pins[0], _frequency_hz)
-    p1.start(_duty_cycle_percent)
-    # set pin as an output pin with optional initial state of HIGH
-    # Note this setup cannot be combined by passing the whole array
-    # as indicated in the NVDIA README
-    # See: https://forums.developer.nvidia.com/t/unable-to-get-multiple-pwm-pins-to-work-simultaneously/284049
-    GPIO.setup(pwm_pins[1], GPIO.OUT, initial=GPIO.HIGH)
-    p2 = GPIO.PWM(pwm_pins[1], _frequency_hz)
-    p2.start(_duty_cycle_percent)
 
     print("PWM running. Press CTRL+C to exit.")
     try:
@@ -35,11 +47,11 @@ def main():
             if _duty_cycle_percent <= 0:
                 incr = -incr
             _duty_cycle_percent += incr
-            p1.ChangeDutyCycle(_duty_cycle_percent)
-            p2.ChangeDutyCycle(_duty_cycle_percent)
+            motor1.change_speed(_duty_cycle_percent);
+            motor2.change_speed(_duty_cycle_percent);
     finally:
-        p1.stop()
-        p2.stop()
+        motor1.turn_off()
+        motor2.turn_off()
         GPIO.cleanup()
 
 if __name__ == '__main__':
